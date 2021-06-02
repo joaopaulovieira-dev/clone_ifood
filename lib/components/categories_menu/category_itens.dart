d4901ae1dcc959afc7cad7316c7c2815f7f09a1e
Crisley Duarte da Silva <crisley,dev@gmail.com>
2021-06-02 00:01:28 -0300

    Tamanho itens da lista

--- lib/components/categories_menu/category_itens.dart
+++ lib/components/categories_menu/category_itens.dart
@@ -5,16 +5,17 @@ import 'package:flutter/material.dart';
 
 Widget itemList(Color cor, String nome) => Stack(children: <Widget>[
       Container(
-        margin: const EdgeInsets.fromLTRB(4, 4, 4, 22),
+        margin: const EdgeInsets.all(4),
         width: 100,
+        height: 100,
         decoration:
             BoxDecoration(color: cor, borderRadius: BorderRadius.circular(16)),
       ),
       Container(
         alignment: Alignment.bottomCenter,
-        margin: const EdgeInsets.only(left: 4),
+        margin: const EdgeInsets.only(left: 4, top: 24),
         width: 100,
-        height: 160,
+        height: 100,
         child: Text(
           '$nome',
           style: TextStyle(
