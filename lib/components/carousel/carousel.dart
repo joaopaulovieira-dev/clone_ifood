0cf91ad7f8e2582ba5b3184cccd3540119c9cff8
Crisley Duarte da Silva <crisley,dev@gmail.com>
2021-06-03 19:30:09 -0300

    Ajustado imagens do carrossel

--- lib/components/carousel/carousel.dart
+++ lib/components/carousel/carousel.dart
@@ -5,16 +5,15 @@ import 'package:flutter_carousel_slider/carousel_slider_indicators.dart';
 import 'package:flutter_carousel_slider/carousel_slider_transforms.dart';
 
 class Carousel extends StatefulWidget {
-
-
-  Carousel({Key ? key,}) : super(key: key);
+  Carousel({
+    Key? key,
+  }) : super(key: key);
 
   @override
   _CarouselState createState() => _CarouselState();
 }
 
 class _CarouselState extends State<Carousel> {
-
   final List<String> letters = [
     AppImages.carousel1,
     AppImages.carousel2,
@@ -40,27 +39,27 @@ class _CarouselState extends State<Carousel> {
             height: 200,
             child: CarouselSlider.builder(
               keepPage: false,
-              unlimitedMode: true,
-              viewportFraction: 0.8,
+              unlimitedMode: false,
+              viewportFraction: 0.9,
               controller: _sliderController,
               slideBuilder: (index) {
                 return Container(
-                  alignment: Alignment.center,
-                  color: Colors.white,
-                  child: Container(
-                    margin: EdgeInsets.all(8),
-                    decoration: BoxDecoration(image: DecorationImage(image: AssetImage(letters[index]),
-                    fit: BoxFit.cover),
-                    borderRadius: BorderRadius.circular(16))
-                ));
+                    alignment: Alignment.center,
+                    color: Colors.white,
+                    child: Container(
+                        margin: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(letters[index]),
+                                fit: BoxFit.fill),
+                            borderRadius: BorderRadius.circular(16))));
               },
               slideTransform: DefaultTransform(),
               slideIndicator: CircularSlideIndicator(
-                itemSpacing: 20,
-                indicatorBorderWidth: 1,
-                alignment: Alignment.bottomCenter,
-                indicatorBorderColor: Colors.black
-              ),
+                  itemSpacing: 20,
+                  indicatorBorderWidth: 1,
+                  alignment: Alignment.bottomCenter,
+                  indicatorBorderColor: Colors.black),
               itemCount: letters.length,
               initialPage: 0,
               enableAutoSlider: false,
